1a29936211e55db72f320d66da1483f2c3c1ff00
Michael Shanosky <134533817+michaelsec@users.noreply.github.com>
2023-05-31 19:29:29 -0400

    Add files via upload

---
+++ socket_server_and_client/socket_client.py
@@ -0,0 +1,23 @@
+import socket
+
+def client_program():
+    host = socket.gethostname()
+    port = 5000
+
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
+
+    while True:
+        message = input("-> ")
+        client_socket.send(message.encode())
+        if message.lower() == "exit":
+            break
+
+        data = client_socket.recv(1024).decode()
+        print("Server response: " + data)
+
+    client_socket.close()
+
+
+if __name__ == '__main__':
+    client_program()
---
+++ socket_server_and_client/socket_server.py
@@ -0,0 +1,25 @@
+import socket
+
+def server_program():
+    host = socket.gethostname()
+    port = 5000
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+    server_socket.listen(2)
+    print("Server started. Waiting for connections...")
+
+    conn, address = server_socket.accept()
+    print("Connected to client: {}".format(address))
+
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        print("From connected client: " + data)
+        message = input('-> ')
+        conn.send(message.encode())
+
+    conn.close()
+
+if __name__ == '__main__':
+    server_program()
